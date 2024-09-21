0a4d9f6d3174119a1e24c07468480beda8109da1
egehanyorulmaz <egehanyorulmaz@uchicago.edu>
2024-09-21 16:31:54 -0500

    Adds handlers and log file save

--- src/search_agent/utils/custom_logging.py
+++ src/search_agent/utils/custom_logging.py
@@ -1,12 +1,19 @@
 import logging
+import sys
 
 
 def setup_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler('search_agent.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
     return logger
